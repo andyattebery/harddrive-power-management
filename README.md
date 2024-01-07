d664e64098a3f92f3a981d7cf9053ee8f4e57b3e
Andy Attebery <me@andyattebery.com>
2024-01-07 15:58:52 -0600

    wip

--- README.md
+++ README.md
@@ -4,13 +4,52 @@
 WIP
 
 ## Requires
-- smartctl
-- hdparm
 - [openSeaChest](https://github.com/Seagate/openSeaChest)
+
+## Devices
+
+`openSeaChest_PowerControl --showEPCSettings` output for devices
+
+### Table Legend
+```
+===EPC Settings===
+* = timer is enabled
+C column = Changeable
+S column = Savable
+All times are in 100 milliseconds
+```
+
+### ST18000NM000J-2TV103 - ZR5BA9B5 - SN02 - ATA
+
+```
+Name       Current Timer Default Timer Saved Timer   Recovery Time C S
+Idle A     *1            *1            *1            1             Y Y
+Idle B     *1200         *1200         *1200         4             Y Y
+Idle C      0             6000          6000         20            Y Y
+Standby Z   0             9000          9000         110           Y Y
+```
+
+### WDC WD180EDGZ-11B2DA0 - 3GKK6M2K - 85.00A85 - ATA
+
+```
+Name       Current Timer Default Timer Saved Timer   Recovery Time C S
+Idle A     *20           *20           *20           1             Y Y
+Idle B     *6000         *6000         *6000         11            Y Y
+Idle C      0             0             0            32            Y Y
+Standby Y   0             0             0            32            Y Y
+Standby Z   0             0             0            85            Y Y
+```
 
 ## References
 - https://superuser.com/questions/555400/what-do-different-values-of-hard-drives-advanced-power-management-feature-hdpa
 - https://wiki.archlinux.org/title/Hdparm#Power_management_configuration
+- https://www.reddit.com/r/truenas/comments/p1ebnf/seagate_exos_load_cyclingidling_info_solution/
+
+### SATA Link Power Management
+- https://www.thomas-krenn.com/en/wiki/SATA_Link_Power_Management
+- https://insanity.industries/post/sata-power-consumption/
+
+### Tools
 - https://www.man7.org/linux/man-pages/man8/hdparm.8.html
 - https://www.smartmontools.org/browser/trunk/smartmontools/smartctl.8.in
 - https://github.com/tyan-boot/wdepc
--- set-harddrive-power-management.sh
+++ set-harddrive-power-management.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-set -- \
-    /dev/disk/by-id/scsi-SATA_WDC_WD140EDGZ-11_9LH51ZBG \
-    /dev/disk/by-id/scsi-SATA_WDC_WD140EDFZ-11_QBH1LSAT \
-    /dev/disk/by-id/scsi-SATA_WDC_WD140EDFZ-11_X1GH3GLL \
-    /dev/disk/by-id/scsi-SATA_ST18000NM003D-3D_ZVTB4SA5 \
-    /dev/disk/by-id/scsi-SATA_WDC_WD180EDGZ-11_8LGD8JRK \
-    /dev/disk/by-id/scsi-SATA_WDC_WD120EMAZ-11_5PGJT29C \
-    /dev/disk/by-id/scsi-SATA_WDC_WD120EMFZ-11_Z2H8HE0T \
-    /dev/disk/by-id/scsi-SATA_WDC_WD140EDFZ-11_9LGA6YWA \
-    /dev/disk/by-id/scsi-SATA_ST14000NM001G-2K_ZL2D3HY7 \
-    /dev/disk/by-id/scsi-SATA_ST14000NM001G-2K_ZL2D12AX \
-    /dev/disk/by-id/scsi-SATA_ST14000NM000J-2T_ZR516PR9 \
-    /dev/disk/by-id/scsi-SATA_ST18000NM000J-2T_ZR5BA9B5 \
-    /dev/disk/by-id/scsi-SATA_WDC_WD180EDGZ-11_3LGHSB2U \
-    /dev/disk/by-id/scsi-SATA_WDC_WD180EDGZ-11_3GKK6M2K
+# set -- \
+#     /dev/disk/by-id/scsi-SATA_WDC_WD140EDGZ-11_9LH51ZBG \
+#     /dev/disk/by-id/scsi-SATA_WDC_WD140EDFZ-11_QBH1LSAT \
+#     /dev/disk/by-id/scsi-SATA_WDC_WD140EDFZ-11_X1GH3GLL \
+#     /dev/disk/by-id/scsi-SATA_ST18000NM003D-3D_ZVTB4SA5 \
+#     /dev/disk/by-id/scsi-SATA_WDC_WD180EDGZ-11_8LGD8JRK \
+#     /dev/disk/by-id/scsi-SATA_WDC_WD120EMAZ-11_5PGJT29C \
+#     /dev/disk/by-id/scsi-SATA_WDC_WD120EMFZ-11_Z2H8HE0T \
+#     /dev/disk/by-id/scsi-SATA_WDC_WD140EDFZ-11_9LGA6YWA \
+#     /dev/disk/by-id/scsi-SATA_ST14000NM001G-2K_ZL2D3HY7 \
+#     /dev/disk/by-id/scsi-SATA_ST14000NM001G-2K_ZL2D12AX \
+#     /dev/disk/by-id/scsi-SATA_ST14000NM000J-2T_ZR516PR9 \
+#     /dev/disk/by-id/scsi-SATA_ST18000NM000J-2T_ZR5BA9B5 \
+#     /dev/disk/by-id/scsi-SATA_WDC_WD180EDGZ-11_3LGHSB2U \
+#     /dev/disk/by-id/scsi-SATA_WDC_WD180EDGZ-11_3GKK6M2K
 
 # set -- \
 #     /dev/disk/by-id/scsi-SATA_ST18000NM003D-3D_ZVTB4SA5 \
@@ -89,15 +89,21 @@ for drive in "$@"; do
     # apm_level=$(hdparm -B $drive | grep APM | cut --delimiter='=' --field=2 | tr --delete '[:space:]')
     
     echo $power_mode
-    # echo "[$apm_level]"
-    echo ---
-    # $openseachest_powercontrol_command --device $drive --idle_c enable
-    # $openseachest_powercontrol_command --device $drive --setAPMLevel 254
-    # $openseachest_powercontrol_command --device $drive --disableAPM
-    # $openseachest_powercontrol_command --device $drive --EPCfeature enable
+
+    # Seagate
     # $openseachest_powercontrol_command --device $drive --idle_a enable
     # $openseachest_powercontrol_command --device $drive --idle_b enable
-    # $openseachest_powercontrol_command --device $drive --idle_c 900000
-    # $openseachest_powercontrol_command --device $drive --idle_c disable
-    # $openseachest_powercontrol_command --device $drive --showEPCSettings
+    # $openseachest_powercontrol_command --device $drive --idle_c enable
+    # $openseachest_powercontrol_command --device $drive --standby_z enable
+
+    # WD
+    # $openseachest_powercontrol_command --device $drive --setAPMLevel 254
+    $openseachest_powercontrol_command --device $drive --disableAPM
+    $openseachest_powercontrol_command --device $drive --EPCfeature enable
+    $openseachest_powercontrol_command --device $drive --idle_a enable
+    $openseachest_powercontrol_command --device $drive --idle_b 300000
+    $openseachest_powercontrol_command --device $drive --idle_c 600000
+    $openseachest_powercontrol_command --device $drive --standby_z 900000
+
+    $openseachest_powercontrol_command --device $drive --showEPCSettings
 done
